df77bf4b9d53839305d3e14c9a94ef9ffd896d78
SARATH DAS <sarathdaskv96@gmail.com>
2024-03-11 21:53:45 +0530

    first commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="flex items-center justify-center ">
+            <h1 className="text-3xl font-bold">Contact Manager</h1>
+        </div>
+    );
+}
+
+export default Header;
